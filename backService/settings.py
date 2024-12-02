fcf3cc52270e42dda0a82ffdc968d81cc334d709
kerrykimwayne <isidortoy@gmail.com>
2024-12-02 08:49:51 +0100

    newss

--- backService/settings.py
+++ backService/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://optimabatis-1.onrender.com']
 
 from datetime import timedelta
 # Application definition
