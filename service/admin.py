037172b48625adf7a400aa8973f021dfcbf87c03
kerrykimwayne <isidortoy@gmail.com>
2024-12-05 00:05:57 +0100

    initialisatio

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Service,SousService,Publiciter,Intervention
 # Register your models here.
+admin.site.register(Service)
+admin.site.register(SousService)
+admin.site.register(Publiciter)
+admin.site.register(Intervention)
--- utilisateur/views.py
+++ utilisateur/views.py
@@ -38,10 +38,10 @@ class RequetepasswordResset(generics.CreateAPIView):
         user.password_reset_token=token
         user.token_created_at = now()
         user.save()
-        reset_url = f"https://optimabatis-1.onrender.com/resetpassword/{token}/"
+        reset_url = f"https://optimabatis-1.onrender.com/passwordget/{token}/"
         send_mail(
                 subject="Réinitialisation de votre mot de passe",
-                message=f"Utilisez ce lien pour réinitialiser votre mot de passe : {reset_url}",
+                message=f"Utilisez ce lien pour réinitialiser votre mot de passe : {reset_url}, code de validation  {token} ",
                 from_email=settings.EMAIL_HOST_USER,
                 recipient_list=[email],fail_silently=False
             )
@@ -53,6 +53,7 @@ class RequetepasswordResset(generics.CreateAPIView):
    
             
 class ConfirmPasswordResset(generics.RetrieveUpdateAPIView):
+    queryset=User.objects.all()
     serializer_class=PasswordResetSerialiser
     def retrieve(self,request,*args,**kwargs):
         return Response({"message":"soumettez votre mot de passe et le jeton "},)
